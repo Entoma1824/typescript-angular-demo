e7f5f7174f09dd13e25f7967c9c827ddb86f6852
Entoma <vienmarcustiongson@gmail.com>
2025-06-18 17:06:38 +0800

    finished chapter 1 of trying to learn angular, interfaces

--- class.ts
+++ class.ts
@@ -1,7 +1,18 @@
-class Employee {
+// import { Login, User } from 'interfaces';
+
+import * as UserLogin from 'interfaces'
+
+interface Adress {
+    street: string;
+    city: string;
+    state: string;
+    pin: string;
+}
+
+class Employee implements UserLogin.Login{
     #id: number;
     protected name:string
-    address : string;
+    address : Adress
 
     get empId() : number {
         return this.#id;
@@ -15,17 +26,23 @@ class Employee {
         return 50;
     }
 
-    constructor(id:number, name:string, address:string) {
+    constructor(id:number, name:string, address: Adress) {
         this.address = address;
         this.#id = id;
         this.name = name;
+    }
+
+    login(): UserLogin.User {
+        // throw new Error('Method not implemented.');
+        return {name: 'John', id:1, email:""}
+
     }
     getNameWithAdress() :string {
         return `${this.name} stays at ${this.address}`;
     }
 }
 
-let john= new Employee(1, "John", "Highway 71");
+let john= new Employee(1, "John", { street: "ABC", city: "Bangalore", state : "Karanataka", pin: "560076"});
 
 john.empId = 100;
 console.log(john.empId)
@@ -34,7 +51,7 @@ console.log(john.empId)
 
 class Manager extends Employee{
 
-    constructor(id:number, name:string, address:string) {
+    constructor(id:number, name:string, address:{ street: "ABC", city: "Bangalore", state : "Karanataka", pin: "560076"}) {
         super(id, name, address);
     }
     getNameWithAdress(): string {
@@ -50,5 +67,5 @@ let address = john.getNameWithAdress();
 
 console.log(address);
 
-let mike = new Manager(2,"Mike", "Cherise Drive")
+let mike = new Manager(2,"Mike", { street: "ABC", city: "Bangalore", state : "Karanataka", pin: "560076"})
 console.log(mike.getNameWithAdress());
